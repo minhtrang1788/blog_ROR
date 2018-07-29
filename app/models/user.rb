e7e99016279d222c16494772329d974b57555d4f
DanTrang <minhtrang1788@gmail.com>
2018-07-29 12:03:14 +0700

    edit index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    validates :email, length: { in: 2 .. 10,
-        too_long: " characters is the maximum allowed" }, presence: true
+    validates :name, :presence: true
+    validates :email, length: { in: 4 .. 255,
+        too_long: " characters is the maximum allowed" }, presence: true , uniqueness: {casensitive: false},
     validate :trang_valid
      def trang_valid
         if name.present? && name != "Trang"
